bdf5682ca19de32632b273cfe990077b48c0c507
Benjamin Brandon <benb89.bb@gmail.com>
2024-02-14 03:14:01 -0500

    added async fetch request to base weather api with successful response

--- src/app/Weather_App/index.tsx
+++ src/app/Weather_App/index.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 import daySvg from '../../../public/day.svg';
 
 const Weather_App = () => {
-  const [userLatitude, setUserLatitude] = useState<number>();
-  const [userLongitude, setUserLongitude] = useState<number>();
-  const [weatherCode, setWeatherCode] = useState();
+  const [userLatitude, setUserLatitude] = useState<number>(0);
+  const [userLongitude, setUserLongitude] = useState<number>(0);
+  const [weather, setWeather] = useState();
 
   useEffect(() => {
     const getLocation = navigator.geolocation.getCurrentPosition(position => {
@@ -22,6 +22,18 @@ const Weather_App = () => {
       console.log('userLong is :', userLongitude);
     });
   });
+
+  useEffect(() => {
+    const getWeather = async () => {
+      const query = await fetch(
+        'https://api.open-meteo.com/v1/forecast?latitude=35.9606&longitude=-83.9207&hourly=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch'
+      );
+      const response = await query.json();
+      console.log('response: ', response);
+      setWeather(response);
+    };
+    getWeather();
+  }, []);
 
   const SunSvg = () => {
     return (
