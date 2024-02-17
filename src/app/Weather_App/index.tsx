60aece1989ef2b60ff16170cd65149624489a24d
Benjamin Brandon <benb89.bb@gmail.com>
2024-02-17 13:25:08 -0500

    tinkering with the fetch

--- src/app/Weather_App/index.tsx
+++ src/app/Weather_App/index.tsx
@@ -11,22 +11,20 @@ const Weather_App = () => {
     const getLocation = async () => {
       navigator.geolocation.getCurrentPosition(
         position => {
-          let location = {
-            lat: position.coords.latitude,
-            long: position.coords.longitude,
-          };
-          return location;
+          setUserLatitude(position.coords.latitude);
+          setUserLongitude(position.coords.longitude);
         },
         error => {
           console.log(error);
         }
       );
+      console.log('user Lat: ', userLatitude, 'user Long: ', userLongitude);
     };
 
     const getWeather = async () => {
       await getLocation();
       const query = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`
+        `https://api.open-meteo.com/v1/forecast?latitude=${userLatitude}&longitude=${userLongitude}&hourly=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`
       )
         .then(res => res.json)
         .then(data => setWeather(data));
